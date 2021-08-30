3d650618c1a34341ddf9a86636668a46d72bfe97
junehay <ndrun@naver.com>
2021-08-30 22:09:43 +0900

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('API document')
+    .setDescription('NestJS API document')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  const port = 3000;
+  await app.listen(port, () => console.log(`server on: ${port}`));
 }
 
 void bootstrap();
